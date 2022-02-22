0a3b7c0c608e635cdc3ea6bd8a4d845bee08327a
Peter <peter@enabledsoft.com>
2022-02-22 14:47:09 +0200

    Updated service name

--- server/ui.go
+++ server/ui.go
@@ -136,7 +136,7 @@ func transactHandler(w http.ResponseWriter, r *http.Request) *appError {
 		return appErrorf(err, "could not read config file: %v", err)
 	}
 
-	serviceStr := "facebook"
+	serviceStr := "Facebook"
 	_, service, err := serviceFromRequest(serviceStr, c)
 	if err != nil {
 		http.Redirect(w, r, "https://connect.adswerve.com/unifier?error=Service%20not%20setup", http.StatusSeeOther)
